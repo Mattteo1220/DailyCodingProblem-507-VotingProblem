0755bb4ed2659347e11ae8e186a675510542efa4
matt Mcentire <Msmcentire1220@outlook.com>
2024-11-03 07:15:28 -0700

    Finish up challenge 507 and display top three winners and who the overall winner is

--- 507/Voting.Domain/VotingService.cs
+++ 507/Voting.Domain/VotingService.cs
@@ -12,6 +12,7 @@ namespace Voting.Domain
     {
         private IDiagnosticLogger _logger;
         private const string FileNameConst = "VoterRecord";
+        private HashSet<VoterRecord> _records;
 
         public VotingService(IDiagnosticLogger logger)
         {
@@ -24,44 +25,73 @@ namespace Voting.Domain
 
             var filePath = GetFilePath();
 
+            var files = GetFiles(filePath);
+
+            _records = new HashSet<VoterRecord>(new VoterIdEqualityComparer());
+
+            foreach(var file in files)
+            {
+               ProcessFile(file);
+            } 
+
+            TabulateVotes();
+
+            _logger.Info($"{nameof(VotingService)}::{nameof(Run)} ending...");
+        }
+
+        private void TabulateVotes()
+        {
+            var candidatesByVotes = _records.GroupBy(x => x.CandidateId)
+                .OrderByDescending(x => x.Select(x => x.VoterId).Count())
+                .Take(3)
+                .ToList();
+
+            foreach(var candidate in candidatesByVotes)
+            {
+                var numOfCounts = candidate.Where(s => s.CandidateId == candidate.Key).Select(x => x.VoterId).Count();
+
+                _logger.Info($"{candidate.Key} has {numOfCounts} votes");
+            }
+
+            _logger.Info($"Candidate: {candidatesByVotes.First().Key} is the winner with {candidatesByVotes.First().Count()} votes");
+        }
+
+        private void ProcessFile(string file)
+        {
+            using (var fs = File.OpenRead(file))
+            {
+                using (var reader = new StreamReader(fs))
+                {
+                    var recordData = string.Empty;
+                    while ((recordData = reader.ReadLine()) != null)
+                    {
+                        if (recordData == null)
+                        {
+                            _logger.Error($"No data in file to process.");
+                            return;
+                        }
+                        var voterId = recordData.Split('|')[0];
+                        var candidateId = recordData.Split('|')[1];
+                        if (!_records.Add(new VoterRecord(voterId, candidateId)))
+                        {
+                            _logger.Error($"FRAUD: {voterId} was present twice in this file. Voting more than once is legally forbidden.");
+                            //potentially add to a db for fraud team to investigate
+                        }
+                    }
+                }
+            }
+        }
+
+        private List<string> GetFiles(string filePath)
+        {
             var files = Directory.GetFiles(filePath);
             if (!files.Any(f => f.Contains(FileNameConst)))
             {
                 _logger.Info($"No files to read.");
             }
 
-            var records = new HashSet<VoterRecord>(new VoterIdEqualityComparer());
-
-            foreach(var file in files)
-            {
-                using (var fs = File.OpenRead(file))
-                {
-                    using (var reader = new StreamReader(fs))
-                    {
-                        var recordData = string.Empty;
-                        while((recordData = reader.ReadLine()) != null)
-                        {
-                            if (recordData == null)
-                            {
-                                _logger.Error($"No data in file to process.");
-                                return;
-                            }
-                            var voterId = recordData.Split('|')[0];
-                            var candidateId = recordData.Split('|')[1];
-                            if (!records.Add(new VoterRecord(voterId, candidateId)))
-                            {
-                                _logger.Error($"FRAUD: {voterId} was present twice in this file. Voting more than once is legally forbidden.");
-                                //potentially add to a db for fraud team to investigate
-                            }
-                        }
-                    }
-                }
-            } 
-
-            _logger.Info($"{nameof(VotingService)}::{nameof(Run)} ending...");
+            return files.ToList();
         }
-
-
         private string GetFilePath()
         {
             return $"c:/src/projects/DailyCodingProblems/507/";
